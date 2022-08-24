4751c8ee1f12b251c0a5418accf0c1e786372f46
Tim Korolev <timopheykor@gmail.com>
2022-08-24 14:07:48 -0500

    gin import, gin example code

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Launching Bookworm CRUD Service...")
+
+	// Gin Quickstart Code Example:
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
