de4938fca19563c73847fbc2bbcd2350d7c5dd2d
Tim Korolev <timopheykor@gmail.com>
2022-08-25 10:36:49 -0500

    Gin testing

--- main.go
+++ main.go
@@ -7,15 +7,41 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Simple structures for testing Gin & JSON marshalling
+type bookData struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Desc   string `json:"description"`
+}
+type booksList struct {
+	Books map[string]bookData `json:"books"`
+}
+
+// getBook takes the name of a book from the request context, and returns
+// a JSON response or an error based on if the book exists in the dataset or not.
+func (bl *booksList) getBook(c *gin.Context) {
+	name := c.Param("name")
+	book, ok := bl.Books[name]
+	if ok {
+		c.JSONP(http.StatusOK, book)
+		return
+	}
+	c.Status(http.StatusNotFound)
+}
+
 func main() {
 	fmt.Println("Launching Bookworm CRUD Service...")
 
-	// Gin Quickstart Code Example:
+	// Some local test data to experiment with
+	bl := booksList{Books: make(map[string]bookData)}
+	b0 := bookData{Title: "Luigi", Author: "Nintendo", Desc: "Ghosts in a mansion, get the vacuum cleaner!"}
+	bl.Books["Luigi"] = b0
+
+	// Create a gin router "r" with default middleware
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+
+	// Define a function to listen for a GET request on a book name
+	r.GET("/book/:name", bl.getBook)
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
